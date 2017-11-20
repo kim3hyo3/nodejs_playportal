2d358e7aaa75df8a28021b737543d676c64415bd
hyojin kim <kim3hyo3@gmail.com>
2017-11-21 00:02:52 +0900

    bootstrap-repair

---
+++ AccountManage/views/culture.js
@@ -0,0 +1,8 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="col-md-3 list-group">
+      <a href="#" class="list-group-item active">
+      외부링크
+      </a>
+      <div class="well well-sm">
