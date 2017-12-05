9deb113f5b55b22d364db9aa867b40ebafd082eb
hjkim <kim3hyo3@gmail.com>
2017-12-05 11:18:56 +0900

    request_update

--- playerportal/routes/request.js
+++ playerportal/routes/request.js
@@ -12,23 +12,8 @@ router.get('/list/:page', function (req, res, next) {
   pool.getConnection(function(err,connection) {
     connection.query('SELECT id_request, m_cd, title, content, date_format(modidate,\'%Y-%m-%d %H:%i:%s\') AS modidate, hit from request;', function (err, rows) {
       if (err) console.error("err : " + err);
-      console.log("rows : " + JSON.stringify(rows));
+      // console.log("rows : " + JSON.stringify(rows));
       res.render('request/list', {token: req.session.username, rows: rows});
-      connection.release();
-    });
-  });
-});
-
-router.get('/detail/:id_request', function (req, res, next) {
-  console.log("aaaaa"+req.params.id_request);
-  var id_request = req.params.id_request;
-
-  pool.getConnection(function(err,connection) {
-  var query = 'SELECT '+id_request+', title, content, date_format(modidate,\'%Y-%m-%d %H:%i:%s\') AS modidate, hit from request where id_request;'
-    connection.query(query, function (err, rows) {
-      if (err) console.error("err : " + err);
-      console.log("rows : " + JSON.stringify(rows));
-      res.render('request/detail', {token: req.session.username, readItem: rows});
       connection.release();
     });
   });
@@ -36,16 +21,15 @@ router.get('/detail/:id_request', function (req, res, next) {
 
 router.post('/write', function(req, res, next){
   console.log(req.body);
-  var password = req.body.password;
   var type =  req.body.type;
   var title = req.body.title;
   var content = req.body.content;
-  var reqData = [password, type, title, content];
+  var reqData = [type, title, content];
 
   pool.getConnection(function (err, connection)
   {
     // Use the connection
-    var sqlForInsertRequest = "insert into request(password, type, title, content) values(?,?,?,?);";
+    var sqlForInsertRequest = "INSERT into request(type, title, content) values(?,?,?);";
     connection.query(sqlForInsertRequest, reqData, function (err, rows) {
       if (err) console.error("err : " + err);
       // console.log("rows : " + JSON.stringify(rows));
@@ -56,5 +40,25 @@ router.post('/write', function(req, res, next){
   });
 });
 
+router.post('/update', function(req, res, next){
+  console.log(req.body);
+  var id_request = req.body.id_request;
+  var type =  req.body.type;
+  var title = req.body.title;
+  var content = req.body.content;
+  var reqData = [id_request, type, title, content, id_request];
+  pool.getConnection(function (err, connection)
+  {
+    // Use the connection
+    var sqlForUpdateRequest = "update request set id_request=?, type=?, title=?, content=?, modidate=now() where id_request=?;";
+    connection.query(sqlForUpdateRequest, reqData, function (err, rows) {
+      if (err) console.error("err : " + err);
+      console.log("rows : " + JSON.stringify(rows));
+      res.redirect('/request');
+      connection.release();
+      // Don't use the connection here, it has been returned to the pool.
+    });
+  });
+});
 
 module.exports = router;
