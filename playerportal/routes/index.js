95277d96e0d955b34a0cca6b987b42932486130f
hyojin kim <kim3hyo3@gmail.com>
2017-12-13 11:51:52 +0900

    routes/index.js-login테스트 index/main.ejs-문구수정 nav.ejs-login테스트

--- playerportal/routes/index.js
+++ playerportal/routes/index.js
@@ -37,19 +37,22 @@ sql = connection.format(sql, inserts);*/
 // sql = 'SELECT m_id, m_password from member where m_id = "'+mid+'"';
     mid = req.body.username;
     mpassword = req.body.password;
-    connection.query('SELECT m_id, m_password from member where m_id = ?', [mid], function (err, rows, fields) {
+    connection.query('SELECT m_id, m_cd, m_password from member where m_id = ?', [mid], function (err, rows, fields) {
      console.log('rows.length '+rows.length);
      //통과구문
      if (rows.length !== 0) {
         console.log('question id '+mid);
         console.log('question password '+mpassword);
-        console.log(rows[0].m_id);
-        console.log(rows[0].m_password);
+        console.log('m_id '+rows[0].m_id);
+        console.log('m_cd '+rows[0].m_cd);
+        console.log('m_password '+rows[0].m_password);
         //통과로직
         if (rows[0].m_id === mid && rows[0].m_password === mpassword) {
+          //sql로 받아온 m_id를 session.loginid에 넣음. m_cd도 같이 넣음.
           req.session.loginid = rows[0].m_id;
+          req.session.logincd = rows[0].m_cd;
           console.log(req.session);
-          res.render('index/main', {token: req.session.loginid});
+          res.render('index/main', {loginid: req.session.loginid},{logincd: req.session.logincd});
           connection.release();
         //실패로직
         } else if (rows[0].m_id !== mid || rows[0].m_password !== mpassword) {
