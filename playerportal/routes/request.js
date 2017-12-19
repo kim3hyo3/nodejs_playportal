a6e604b2c29dec661ebb03160d107c0ecafada4b
hyojin kim <kim3hyo3@gmail.com>
2017-12-19 21:57:20 +0900

    routes/request.js-ModifyListSQLquery v/l/head.ejs-AddSelectCDN v/r/rqst_list.ejs-ModifyFrontEnd

--- playerportal/routes/request.js
+++ playerportal/routes/request.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
 });
 */
 
+//글목록 보기
 router.get('/', function(req, res, next) {
   //console.log('Log_loginid'+req.session.loginid);
   //console.log(req.session);
@@ -20,10 +21,11 @@ router.get('/', function(req, res, next) {
 */
   pool.getConnection(function(err,connection) {
     // SELECT id_request, title, content, date_format(regdate,'%Y-%m-%d %H:%i:%s')
-    connection.query('SELECT id_request, title, content, date_format(regdate,\'%Y-%m-%d %H:%i\') AS regdate, hit, member.m_name, member.m_type, type.type_name ' +
+    connection.query('SELECT id_request, title, content, date_format(regdate,\'%Y-%m-%d %H:%i\') AS regdate, hit, member.m_name, member.m_type, task_type.type_name, task_type.m_cd, task_status.status_name, task_status.status_option ' +
       'FROM request_board ' +
       'INNER join member on request_board.m_cd = member.m_cd ' +
-      'INNER join type on request_board.type_cd = type.type_cd ' +
+      'INNER join task_type on request_board.type_cd = task_type.type_cd ' +
+      'INNER join task_status on request_board.status_cd = task_status.status_cd ' +
       'ORDER BY id_request DESC;',
       function (err, rows) {
       if (err) console.error("err : " + err);
@@ -61,16 +63,17 @@ router.post('/edit', function(req, res, next){
   console.log(req.body);
   var id_request = req.body.id_request;
   var type_cd =  req.body.type_cd;
+  var status_cd = req.body.status_cd;
   var title = req.body.title;
   var content = req.body.content;
   //session에서 logincd을 받아와서 m_cd에 넣음.
   var m_cd = req.session.logincd;
-  var reqData = [id_request, type_cd, title, content, m_cd, id_request];
+  var reqData = [id_request, type_cd, status_cd, title, content, m_cd, id_request];
   console.log(reqData);
   pool.getConnection(function (err, connection)
   {
     // Use the connection
-    var sqlForEditRequest = "UPDATE request_board set id_request=?, type_cd=?, title=?, content=?, m_cd=?, editdate=now() WHERE id_request=?;";
+    var sqlForEditRequest = "UPDATE request_board set id_request=?, type_cd=?, status_cd=?, title=?, content=?, m_cd=?, editdate=now() WHERE id_request=?;";
     connection.query(sqlForEditRequest, reqData, function (err, rows) {
       if (err) console.error("err : " + err);
       // console.log("rows : " + JSON.stringify(rows));
@@ -96,7 +99,6 @@ router.post('/delete', function(req, res, next){
       // Don't use the connection here, it has been returned to the pool.
     });
   });
-
 });
 
 module.exports = router;
