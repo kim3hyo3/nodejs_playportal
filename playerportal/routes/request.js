ace906576e89d9f5d4f4ba898ab3bc550d9d3a18
hyojin kim <kim3hyo3@gmail.com>
2017-12-27 16:34:13 +0900

    요청게시판-글쓰기기능-순서변경 r/request.js-변수순서변경

--- playerportal/routes/request.js
+++ playerportal/routes/request.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   pool.getConnection(function (err, connection) {
     // SELECT id_request, title, content, date_format(regdate,'%Y-%m-%d %H:%i:%s')
-    connection.query('SELECT id_request, title, content, date_format(regdate,\'%Y-%m-%d %H:%i\') AS regdate, hit, ' +
+    connection.query('SELECT id_request, title, content, hit, date_format(regdate,\'%Y-%m-%d %H:%i\') AS regdate, ' +
       'member.m_name, member.m_type, ' +
       'task_type.type_cd, task_type.type_name, task_type.mng_name, ' +
       'task_status.status_name, task_status.status_option ' +
@@ -25,10 +25,9 @@ router.get('/', function (req, res, next) {
   });
 });
 
-// Original
+// original
 router.post('/write', function(req, res, next){
   console.log(req.body);
-
   var title = req.body.title;
   var content = req.body.content;
   //session에서 logincd을 받아와서 m_cd에 넣음.
@@ -52,45 +51,20 @@ router.post('/write', function(req, res, next){
   });
 });
 
-/*
-// Upgrade02
-router.post('/write', function(req, res, next){
-  //post로 req.body로 받아옴.
-  var title = req.body.title;
-  var content = req.body.content;
-  //session에서 logincd 받고 m_cd에 넣음.
-  var m_cd = req.session.logincd;
-  var status_cd = req.body.status_cd;
-  var type_cd = req.body.type_cd;
-  var reqData01 = [title, content, m_cd, status_cd, type_cd];
-  var sql01 = 'INSERT INTO request_board(title, content, m_cd, status_cd, type_id) values(?,?,?,?,?)' +
-    // '(SELECT alias.* FROM (SELECT DISTINCT task_type.type_id FROM task_type INNER JOIN request_board ON request_board.type_id = task_type.type_id WHERE task_type.type_cd = ? )alias))'
-  pool.getConnection(function (err, connection) {
-    connection.query(sql01, reqData01, function (err, rslt01) {
-      if (err) console.error("err : " + err);
-      // console.log("rows : " + JSON.stringify(rows));
-      res.redirect('/request');
-      connection.release();
-      // Don't use the connection here, it has been returned to the pool.
-    });
-  });
-});
-*/
-
 router.post('/edit', function (req, res, next) {
   console.log(req.body);
   var id_request = req.body.id_request;
-  var type_cd = req.body.type_cd;
-  var status_cd = req.body.status_cd;
   var title = req.body.title;
   var content = req.body.content;
   //session에서 logincd을 받아와서 m_cd에 넣음.
   var m_cd = req.session.logincd;
-  var reqData = [id_request, type_cd, status_cd, title, content, m_cd, id_request];
+  var status_cd = req.body.status_cd;
+  var type_cd = req.body.type_cd;
+  var reqData = [id_request, title, content, m_cd, status_cd, type_cd, id_request];
   console.log(reqData);
   pool.getConnection(function (err, connection) {
     // Use the connection
-    var sqlForEditRequest = "UPDATE request_board set id_request=?, type_cd=?, status_cd=?, title=?, content=?, m_cd=?, editdate=now() WHERE id_request=?;";
+    var sqlForEditRequest = "UPDATE request_board set id_request=?, title=?, content=?, m_cd=?, status_cd=?, type_cd=?, editdate=now() WHERE id_request=?;";
     connection.query(sqlForEditRequest, reqData, function (err, rows) {
       if (err) console.error("err : " + err);
       // console.log("rows : " + JSON.stringify(rows));
